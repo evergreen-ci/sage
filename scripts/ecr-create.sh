923abf62b1bdac22c137cac786bf28591bf0c129
malik.hadjri <malik.hadjri@mongodb.com>
2025-07-15 13:57:42 -0400

    change arch?

--- scripts/ecr-create.sh
+++ scripts/ecr-create.sh
@@ -6,4 +6,4 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 . $parent_path/../.env
 
 
-aws ecr create-repository --repository-name devprod-evergreen/evergreen-ai-service
+aws ecr create-repository --repository-name devprod-evergreen/sage
--- scripts/secrets.sh
+++ scripts/secrets.sh
@@ -5,5 +5,5 @@ skunkube="kubectl --context=api.staging.corp.mongodb.com --namespace=devprod-eve
 AWS_ACCESS_KEY_ID=$($skunkube get secret ecr -o jsonpath="{.data.ecr_access_key}" | base64 --decode && echo)
 AWS_SECRET_ACCESS_KEY=$($skunkube get secret ecr -o jsonpath="{.data.ecr_secret_key}" | base64 --decode && echo)
 
-export AWS_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY
+echo $AWS_ACCESS_KEY_ID
+echo $AWS_SECRET_ACCESS_KEY
