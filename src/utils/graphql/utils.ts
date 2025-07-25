66bf629f02d13ce214443b7338c3fa9528e894ac
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-07-25 00:53:14 -0400

    Fix module resolution for graphql file loading

--- src/utils/graphql/utils.ts
+++ src/utils/graphql/utils.ts
@@ -1,18 +1,21 @@
 import { createTool, Tool } from '@mastra/core';
+import { z } from 'zod';
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
 import { logger } from '../logger';
 import { evergreenGraphQLClient } from './evergreenGraphQLClient';
-import { z } from "zod";
 
-// __dirname is automatically available in CommonJS environments
 /**
  * Loads a GraphQL file from the given path
  * @param path - The path to the GraphQL file
  * @returns The contents of the GraphQL file
  */
 export const loadGraphQLFile = (path: string) => {
-    const resolvedPath = resolve("./src", path);
+  // TODO: This is a hack to get the correct path to the GraphQL file
+  // When running in the mastra directory, we need to go up 2 levels to get to the src directory
+  // This is required due to a mismatch in how mastra and the express server handle module resolution
+  const isInMastra = process.cwd().includes('.mastra');
+  const resolvedPath = resolve(isInMastra ? '../../src' : './src', path);
   let file = '';
   try {
     file = readFileSync(resolvedPath, 'utf8');
@@ -25,37 +28,43 @@ export const loadGraphQLFile = (path: string) => {
   return file;
 };
 
-
-interface GraphQLToolInput<TSchema extends z.ZodObject<any>> extends Tool<TSchema> {
+interface GraphQLToolInput<TSchema extends z.ZodObject<any>>
+  extends Tool<TSchema> {
   query: string;
   id: string;
-  inputSchema: TSchema;
+  inputSchema: z.ZodObject<any>;
   description: string;
 }
 
 /**
  * Creates a Mastra tool from a GraphQL query and input schema
  * @param param0 - The input schema
+ * @param param0.query - The GraphQL query
+ * @param param0.id - The ID of the tool
+ * @param param0.inputSchema - The input schema
+ * @param param0.description - The description of the tool
  * @returns A Mastra tool
  */
-export const createGraphQLTool = <
-  TSchema extends z.ZodObject<any>
->({
-  query,
+export const createGraphQLTool = <TSchema extends z.ZodObject<any>>({
+  description,
   id,
   inputSchema,
-  description,
-}: GraphQLToolInput<TSchema>) => {
-  return createTool({
+  query,
+}: GraphQLToolInput<TSchema>) =>
+  createTool({
     id,
     inputSchema,
     description,
     execute: async ({ context }) => {
-      const result = await evergreenGraphQLClient.executeQuery(
-        query,
-        context
-      );
-      return result;
+      try {
+        const result = await evergreenGraphQLClient.executeQuery(
+          query,
+          context
+        );
+        return result;
+      } catch (error) {
+        logger.error(`Error executing GraphQL query: ${error}`);
+        throw error;
+      }
     },
   });
-};
