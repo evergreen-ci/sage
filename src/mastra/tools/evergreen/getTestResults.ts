f6be706bd4d00e29de40f1358539ec940b4d8efc
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-07-25 15:49:55 -0400

    Extract out inputSchema

--- src/mastra/tools/evergreen/getTestResults.ts
+++ src/mastra/tools/evergreen/getTestResults.ts
@@ -1,3 +1,4 @@
+import { ToolExecutionContext } from '@mastra/core';
 import { z } from 'zod';
 import {
   createGraphQLTool,
@@ -8,16 +9,21 @@ const GET_TEST_RESULTS = loadGraphQLFile(
   'gql/queries/get-test-results.graphql'
 );
 
-export const getTestResults = createGraphQLTool({
+const inputSchema = z.object({
+  taskID: z.string(),
+  execution: z.number().optional().default(0),
+  pageNum: z.number().optional().default(0),
+  limitNum: z.number().optional().default(50),
+  statusList: z.array(z.string()).optional().default(['fail']),
+  testName: z.string().optional().default(''),
+});
+
+export const getTestResults = createGraphQLTool<
+  typeof inputSchema,
+  ToolExecutionContext<typeof inputSchema>
+>({
   query: GET_TEST_RESULTS,
   id: 'Get Test Results',
   description: `Fetches the test results for a given taskID`,
-  inputSchema: z.object({
-    taskID: z.string(),
-    execution: z.number().optional().default(0),
-    pageNum: z.number().optional().default(0),
-    limitNum: z.number().optional().default(50),
-    statusList: z.array(z.string()).optional().default(['fail']),
-    testName: z.string().optional().default(''),
-  }),
+  inputSchema,
 });
