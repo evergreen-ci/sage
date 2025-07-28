2cb6805a1a1f10e97e47a650d8ce4a23f904424a
malik.hadjri <malik.hadjri@mongodb.com>
2025-07-28 16:28:05 -0400

    Try different approach to otel

--- src/tracing.ts
+++ src/tracing.ts
@@ -2,9 +2,7 @@ import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentation
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { resourceFromAttributes } from '@opentelemetry/resources';
 import { NodeSDK } from '@opentelemetry/sdk-node';
-import {
-  SEMRESATTRS_SERVICE_NAME
-} from "@opentelemetry/semantic-conventions/build/src/resource/SemanticResourceAttributes";
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 
 const traceExporter = new OTLPTraceExporter({
   url: 'https://otel-collector.staging.corp.mongodb.com:443/v1/traces',
@@ -12,7 +10,7 @@ const traceExporter = new OTLPTraceExporter({
 
 const sdk = new NodeSDK({
   resource: resourceFromAttributes({
-    [SEMRESATTRS_SERVICE_NAME]: 'sage',
+    [SemanticResourceAttributes.SERVICE_NAME]: 'sage',
   }),
   traceExporter,
   instrumentations: [getNodeAutoInstrumentations()],
