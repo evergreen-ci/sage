00b02266b99b3eb5ecf40ca0607515a958121d82
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-07-29 22:13:46 -0400

    Create thread and working memory for parsley agent

--- src/mastra/agents/parsleyAgent.ts
+++ src/mastra/agents/parsleyAgent.ts
@@ -1,6 +1,35 @@
 import { Agent } from '@mastra/core/agent';
+import { Memory } from '@mastra/memory';
 import { gpt41Nano } from '../models/openAI/gpt41';
 import getTaskTool from '../tools/evergreen/getTask';
+import { memoryStore } from '../utils/memory';
+
+const parsleyMemory = new Memory({
+  storage: memoryStore,
+  options: {
+    workingMemory: {
+      // TODO: Memory is scoped to the thread, so we will only recall from the current chat window.
+      scope: 'thread',
+      enabled: true,
+      // Define the template for the working memory
+      // The working memory is like a notepad that the agent can use to remember information
+      template: `
+      # Task Information
+      - Task ID: {{taskId}}
+      - Execution: {{execution}}
+      - Task Name: {{displayName}}
+      - Task Status: {{displayStatus}}
+      - Version: {{version}}
+      - Build Variant: {{buildVariant}}
+      - Patch Number: {{patchNumber}}
+      - Details: {{details}}
+
+      # Other interesting information
+      - {{otherInformation}}
+      `,
+    },
+  },
+});
 
 export const parsleyAgent: Agent = new Agent({
   name: 'Parsley Agent',
@@ -8,12 +37,12 @@ export const parsleyAgent: Agent = new Agent({
     'Parsley is a helpful assistant that can help with tasks questions when embedded in the parsley log viewer',
   instructions: `
    You are parsley ai, a helpful assistant that can help with tasks and questions. You are able to use the following tools:
-    - get_task: Get information about an evergreen task
-    You are currently in the context of the following task:
+   You are currently in the context of the following task:
 `,
   model: gpt41Nano,
   tools: {
     getTask: getTaskTool,
   },
+  memory: parsleyMemory,
   workflows: {},
 });
