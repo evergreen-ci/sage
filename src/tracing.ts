32badd7fd6697e002b376f7019cb50e8816cfae0
malik.hadjri <malik.hadjri@mongodb.com>
2025-07-31 12:46:21 -0400

    CR

--- src/tracing.ts
+++ src/tracing.ts
@@ -4,19 +4,16 @@ import { resourceFromAttributes } from '@opentelemetry/resources';
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { AlwaysOnSampler } from '@opentelemetry/sdk-trace-base';
 import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
-import { getEnvVar } from 'config';
+import { config } from 'config';
 
 const traceExporter = new OTLPTraceExporter({
-  url: getEnvVar(
-    'OTEL_COLLECTOR_URL',
-    'http://otel-collector-web-app.devprod-platform.svc.cluster.local:4318/v1/traces'
-  ),
+  url: config.otelCollectorURL,
 });
 
 const sdk = new NodeSDK({
   resource: resourceFromAttributes({
     [ATTR_SERVICE_NAME]: 'sage',
-    ['environment']: getEnvVar('DEPLOYMENT_ENV', 'staging'),
+    ['environment']: config.deploymentEnv,
   }),
   traceExporter: traceExporter,
   instrumentations: [getNodeAutoInstrumentations()],
