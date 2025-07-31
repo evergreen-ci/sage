abc3ddaed3644bb4d09641fa6b923117fe056f91
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-07-31 17:44:10 -0400

    Rename to conversations route

--- src/api-server/routes/completions/index.ts
+++ src/api-server/routes/completions/index.ts
@@ -4,6 +4,6 @@ import parsleyRouter from './parsley';
 // export express router
 const completionsRouter = express.Router();
 
-completionsRouter.use('/parsley', parsleyRouter);
+completionsRouter.use('/parsley/conversations', parsleyRouter);
 
 export default completionsRouter;
