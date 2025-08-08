1f91ac6d76f704b2a0c7ffa2080b42fe2b6a57f1
malik.hadjri <malik.hadjri@mongodb.com>
2025-08-08 13:55:43 -0400

    New creation route

--- src/api-server/routes/completions/parsley/index.ts
+++ src/api-server/routes/completions/parsley/index.ts
@@ -1,14 +1,34 @@
 import express from 'express';
 import addMessageRoute from './addMessage';
+import createConversationRoute from './createConversation';
 import getMessagesRoute from './getMessages';
 
 // export express router
 const router = express.Router();
 
+export type AddMessageOutput = {
+  message: string;
+  requestId: string;
+  timestamp: string;
+  completionUsage: {
+    promptTokens: number;
+    completionTokens: number;
+    totalTokens: number;
+  };
+  conversationId: string;
+};
+
+export type ErrorResponse = {
+  message: string;
+};
+
+// Route to add a conversation and get a response
+router.post('/messages', createConversationRoute);
+
 // Route to add a new message to a conversation and get a response
-router.post('/messages', addMessageRoute);
+router.post('/:conversationId/messages', addMessageRoute);
 
 // Route to get messages for a conversation
-router.get('/messages', getMessagesRoute);
+router.get('/:conversationId/messages', getMessagesRoute);
 
 export default router;
