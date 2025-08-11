f7f87b1fd8d9106456ddf4e354ffb90fe6e1ee63
malik.hadjri <malik.hadjri@mongodb.com>
2025-08-11 12:49:55 -0400

    Lint

--- src/e2e/root/completions/parsley.test.ts
+++ src/e2e/root/completions/parsley.test.ts
@@ -69,58 +69,6 @@ describe('completions/parsley/conversations/:conversationId/messages', () => {
     expect(secondResponse.body.message).toContain('TEST MESSAGE 123');
   });
 });
-describe('completions/parsley/conversations/messages', () => {
-  const endpoint =
-    '/completions/parsley/conversations/:conversationId/messages';
-  const conversationId = 'null';
-  it('sending a message will return a completion and create a new thread', async () => {
-    const response = await request(app)
-      .post(endpoint.replace(':conversationId', conversationId))
-      .send({
-        message: 'Hello, world!',
-      });
-    expect(response.status).toBe(200);
-    expect(response.body.message).not.toBeNull();
-    expect(response.body.conversationId).not.toBeNull();
-  });
-  it('should return a 400 status code if the message is not provided', async () => {
-    const response = await request(app)
-      .post(endpoint.replace(':conversationId', conversationId))
-      .send({});
-    expect(response.status).toBe(400);
-    expect(response.body.message).toBe('Invalid request body');
-  });
-  it('should return a 400 status code if the conversationId is not null and the conversation does not exist', async () => {
-    const response = await request(app)
-      .post(endpoint.replace(':conversationId', '123'))
-      .send({
-        message: 'Hello, world!',
-      });
-    expect(response.status).toBe(404);
-    expect(response.body.message).toBe('Conversation not found');
-  });
-  it('should remember the previous messages if the conversationId is not null', async () => {
-    const response = await request(app)
-      .post(endpoint.replace(':conversationId', 'null'))
-      .send({
-        message: 'Remember this message: "TEST MESSAGE 123"',
-      });
-    expect(response.status).toBe(200);
-    expect(response.body.message).not.toBeNull();
-    expect(response.body.conversationId).not.toBeNull();
-    const newConversationId = response.body.conversationId;
-
-    const secondResponse = await request(app)
-      .post(endpoint.replace(':conversationId', newConversationId))
-      .send({
-        message:
-          'Print out the content of my last message so we can test that history recollection works this is used in a unit test',
-      });
-    expect(secondResponse.status).toBe(200);
-    expect(secondResponse.body.message).not.toBeNull();
-    expect(secondResponse.body.message).toContain('TEST MESSAGE 123');
-  });
-});
 describe('completions/parsley/conversations/:conversationId/messages', () => {
   const endpoint =
     '/completions/parsley/conversations/:conversationId/messages';
