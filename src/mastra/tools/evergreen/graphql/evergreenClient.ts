7b65e3c1bd800b15f7268986867eaee0d51a99f0
malik.hadjri <malik.hadjri@mongodb.com>
2025-08-13 23:20:48 -0400

    Try auth

--- src/mastra/tools/evergreen/graphql/evergreenClient.ts
+++ src/mastra/tools/evergreen/graphql/evergreenClient.ts
@@ -1,19 +1,9 @@
 import { config } from '../../../../config';
 import { GraphQLClient } from '../../../../utils/graphql/client';
 
-export const createEvergreenClient = (apiUser?: string, apiKey?: string) =>
-  new GraphQLClient(
-    config.evergreen.graphqlEndpoint,
-    config.evergreen.userIDHeader,
-    {
-      ...(apiUser && { 'Api-User': apiUser }),
-      ...(apiKey && { 'Api-Key': apiKey }),
-    }
-  );
-
-const evergreenClient = createEvergreenClient(
-  config.evergreen.apiUser,
-  config.evergreen.apiKey
+const evergreenClient = new GraphQLClient(
+  config.evergreen.graphqlEndpoint,
+  config.evergreen.userIDHeader
 );
 
 export default evergreenClient;
