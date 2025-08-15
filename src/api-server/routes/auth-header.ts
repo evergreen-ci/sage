595fd5f38305bd519b3b9b7899006ea91316da82
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-08-15 19:04:07 -0400

    log rest of headers

--- src/api-server/routes/auth-header.ts
+++ src/api-server/routes/auth-header.ts
@@ -2,9 +2,12 @@ import { Request, Response } from 'express';
 
 const authHeaderRoute = (req: Request, res: Response) => {
   const authHeader = req.headers['x-kanopy-internal-authorization'];
+  const userHeader = req.headers['"X-Kanopy-Authorization"'];
 
   res.json({
     'X-Kanopy-Internal-Authorization': authHeader || null,
+    'X-Kanopy-Authorization': userHeader || null,
+    restHeaders: req.headers,
   });
 };
 
