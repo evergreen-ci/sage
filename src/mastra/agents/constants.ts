3a1dc9a91a3f641d59519cb102820ec6b8c172f3
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-08-22 14:48:12 -0400

    Fix merge conflict

--- src/mastra/agents/constants.ts
+++ src/mastra/agents/constants.ts
@@ -1,9 +1,7 @@
-const PARSLEY_AGENT_NAME = 'parsleyAgent';
-
 /**
  * This is the user id that is used to identify the user in the runtime context.
  * It is used to identify the end user making the request to the downstream API.
  */
 const USER_ID = 'userId';
 
-export { PARSLEY_AGENT_NAME, USER_ID };
+export { USER_ID };
