4e3c60503261300f7c67d59d9ab83c489822997f
minnakt <minna.kt@mongodb.com>
2025-09-05 17:12:10 -0400

    fix: update script to print to task

--- .evergreen/scripts/publish-evals.sh
+++ .evergreen/scripts/publish-evals.sh
@@ -3,7 +3,10 @@
 # Braintrust CLI does not correctly fail if there are compile errors while running evals, so this is handled by the script below.
 
 # Pipe stderr output to stdout, then check for string that indicates failure. 
-if yarn eval:send_to_braintrust $EVAL_DIR 2>&1 | grep -q "Failed to compile"; then
+if yarn eval:send_to_braintrust src/evals/$EVAL_DIR 2>&1 | tee bin/$EVAL_DIR.log  | grep -q "Failed to compile"; then
     echo "ERROR: The eval tests could not run due to a compile failure."
     exit 1
 fi
+
+# Print out the log content in the task.
+cat bin/$EVAL_DIR.log
