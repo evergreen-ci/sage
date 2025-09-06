93f7ab430b56fb972c5d3a1f9907f178437d3abd
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-09-06 14:30:11 -0400

    Ensure thinking agent properly fetches task id

--- src/mastra/agents/planning/sageThinkingAgent.ts
+++ src/mastra/agents/planning/sageThinkingAgent.ts
@@ -1,9 +1,10 @@
 import { Agent } from '@mastra/core/agent';
+import { RuntimeContext } from '@mastra/core/runtime-context';
 import { Memory } from '@mastra/memory';
+
 import { gpt41 } from '../../models/openAI/gpt41';
 import { memoryStore } from '../../utils/memory';
 import { logCoreAnalyzerTool } from '../../workflows/logCoreAnalyzerWorkflow';
-import { USER_ID } from '../constants';
 import { askEvergreenAgentTool } from '../evergreenAgent';
 import { askQuestionClassifierAgentTool } from './questionClassifierAgent';
 
@@ -22,11 +23,7 @@ export const sageThinkingAgent: Agent = new Agent({
   description:
     'A agent that thinks about the user question and decides the next action.',
   memory: sageThinkingAgentMemory,
-  instructions: ({ runtimeContext }) => {
-    const userID = runtimeContext.get(USER_ID);
-    const logMetadata = runtimeContext.get('logMetadata');
-    const logURL = runtimeContext.get('logURL');
-    return `
+  instructions: ({ runtimeContext }) => `
   You are Parsley AI. A senior software engineer that can think about a users questions and decide on a course of action to answer the question. 
   You have a deep understanding of the evergreen platform and have access to a series of tools that can help you answer any question.
 
@@ -47,15 +44,12 @@ export const sageThinkingAgent: Agent = new Agent({
 
   ## Rules
   When answering a user question using markdown avoid using large headings. Keep it simple and concise.
+  When passing ids to agents, Make sure to always pass the full task id, DO NOT pass a shortened task id or truncate it in any way.
   
   <ADDITIONAL_CONTEXT>
-  User ID: ${userID}
-  Log Metadata: ${JSON.stringify(logMetadata)}
-  The Raw log url for the log we are looking at is: ${logURL}
+  ${stringifyRuntimeContext(runtimeContext)}
   </ADDITIONAL_CONTEXT>
-
-  `;
-  },
+  `,
   model: gpt41,
   tools: {
     askQuestionClassifierAgentTool,
@@ -63,3 +57,8 @@ export const sageThinkingAgent: Agent = new Agent({
     logCoreAnalyzerTool,
   },
 });
+
+const stringifyRuntimeContext = (runtimeContext: RuntimeContext) => {
+  const context = runtimeContext.toJSON();
+  return JSON.stringify(context, null, 2);
+};
