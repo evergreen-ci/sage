2530f889eed1289ddb567ca1bcd371a0de3e88d3
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-09-10 14:14:06 -0400

    Remove redundent file logging

--- src/config/index.ts
+++ src/config/index.ts
@@ -19,7 +19,6 @@ export interface Config {
   logging: {
     /** LOG_LEVEL */
     logLevel: string;
-    logToFile: boolean;
   };
   db: {
     /** MONGODB_URI */
@@ -127,7 +126,6 @@ export const config: Config = {
   },
   logging: {
     logLevel: getEnvVar('LOG_LEVEL', 'info'),
-    logToFile: getEnvVar('LOG_TO_FILE', 'true') === 'true',
   },
   aiModels: {
     azure: {
