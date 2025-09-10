668a86d072555114d13c841e9df9d9704ddb1154
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-09-10 14:06:20 -0400

    Setup honeycomb logs export

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,7 +1,8 @@
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-http';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { resourceFromAttributes } from '@opentelemetry/resources';
-import { NodeSDK } from '@opentelemetry/sdk-node';
+import { NodeSDK, logs } from '@opentelemetry/sdk-node';
 import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
 import { BraintrustSpanProcessor } from 'braintrust';
@@ -21,9 +22,17 @@ const otlpExporter = new OTLPTraceExporter({
   },
 });
 
+const otlpLogExporter = new OTLPLogExporter({
+  url: config.honeycomb.otelLogCollectorURL,
+  headers: {
+    'x-honeycomb-team': config.honeycomb.apiKey,
+  },
+});
+
 const sentrySpanProcessor = new SentrySpanProcessor();
 
 const sdk = new NodeSDK({
+  logRecordProcessors: [new logs.SimpleLogRecordProcessor(otlpLogExporter)],
   instrumentations: [getNodeAutoInstrumentations()],
   spanProcessors: [
     braintrustSpanProcessor,
