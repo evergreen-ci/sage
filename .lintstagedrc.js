6225fa77f1e3393a1d49bf506366169f3ffbfb59
minnakt <minna.kt@mongodb.com>
2025-09-17 14:26:49 -0400

    Put extensions back

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,5 +2,5 @@ export default {
   // Run ESLint on TypeScript and JavaScript files
   '*.{ts,tsx,js,jsx}': ['yarn eslint:strict'],
   // Run Prettier formatting check on all supported files
-  '*.{ts,tsx,js,jsx}': ['yarn format:check'],
+  '*.{ts,tsx,js,jsx,json,md}': ['yarn format:check'],
 };
