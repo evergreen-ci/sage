e6848368757822d01fa81aeb8cdfc7e443d2f226
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-09-25 22:25:57 -0400

    Fix import

--- src/mastra/agents/evergreenAgent.ts
+++ src/mastra/agents/evergreenAgent.ts
@@ -1,10 +1,6 @@
 import { Workflow } from '@mastra/core';
 import { Agent } from '@mastra/core/agent';
 import { Memory } from '@mastra/memory';
-import {
-  getTaskHistoryWorkflow,
-  getVersionWorkflow,
-} from 'mastra/workflows/evergreen';
 import { gpt41 } from '../models/openAI/gpt41';
 import {
   getTaskTool,
@@ -17,6 +13,10 @@ import {
   wrapAgentWithTracing,
   wrapToolWithTracing,
 } from '../utils/tracing/wrapWithTracing';
+import {
+  getTaskHistoryWorkflow,
+  getVersionWorkflow,
+} from '../workflows/evergreen';
 
 const evergreenAgentMemory = new Memory({
   storage: memoryStore,
