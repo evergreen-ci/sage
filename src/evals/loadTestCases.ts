105485795bcb97ec1f375624d66cf2cb913ac7a4
minnakt <minna.kt@mongodb.com>
2025-10-01 15:51:34 -0400

    Add comment

--- src/evals/loadTestCases.ts
+++ src/evals/loadTestCases.ts
@@ -1,6 +1,11 @@
 import { initDataset } from 'braintrust';
 import { PROJECT_NAME } from 'evals/constants';
 
+/**
+ * Loads test cases from the Braintrust dataset. Visit Braintrust to view and create new datasets.
+ * @param datasetName - name of the dataset to load
+ * @returns an array of test cases to run in evals
+ */
 export const loadTestCases = async <TestCase>(datasetName: string) => {
   const dataset = initDataset(PROJECT_NAME, {
     dataset: datasetName,
