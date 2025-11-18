1f5b6009e9fd960169bdfe1a10ab51711f7e2ad9
Byron Hood <byron.hood@mongodb.com>
2025-11-18 12:18:52 -0500

    Add evals

---
+++ src/evals/questionOwnershipAgent/reporter.eval.ts
@@ -0,0 +1,13 @@
+import { BaseEvalConfig, createBaseEvalReporter } from '@/evals/baseEval';
+import { ReporterName } from '@/evals/constants';
+import { createScoreChecker } from '@/evals/scorers';
+import { TestCase } from './types';
+
+const createEvalConfig = (): BaseEvalConfig<TestCase> => ({
+  reporterName: ReporterName.SlackQuestionOwnership,
+  testSuiteName: 'Slack Question Ownership Evals',
+  xmlFileOutputName: 'question_ownership_evals',
+  calculateScores: createScoreChecker,
+});
+
+export const reporter = createBaseEvalReporter(createEvalConfig());
