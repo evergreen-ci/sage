bba73562b2f11683c742720a7ac6cac734a73ffb
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-11-19 17:14:40 -0500

    Create error transport

---
+++ src/utils/sentry/sentryIssueTransport.ts
@@ -0,0 +1,42 @@
+import * as Sentry from '@sentry/node';
+import Transport from 'winston-transport';
+
+class SentryIssueTransport extends Transport {
+  log(info: Record<string, unknown>, callback: () => void) {
+    setImmediate(() => this.emit('logged', info));
+
+    const { err, error, level, message, stack, ...extra } = info;
+
+    // Only create Issues for error-level logs
+    if (level === 'error') {
+      // Try to find a real Error object to capture
+      let throwable = null;
+      if (error instanceof Error) {
+        throwable = error;
+      } else if (err instanceof Error) {
+        throwable = err;
+      }
+
+      if (throwable) {
+        Sentry.captureException(throwable, {
+          extra: {
+            message,
+            ...extra,
+          },
+        });
+      } else {
+        // Fall back to a message-based error
+        Sentry.captureMessage(
+          typeof message === 'string' ? message : JSON.stringify(message),
+          {
+            level: 'error',
+            extra,
+          }
+        );
+      }
+    }
+
+    callback();
+  }
+}
+export default SentryIssueTransport;
