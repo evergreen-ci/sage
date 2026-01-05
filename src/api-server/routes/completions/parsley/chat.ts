845c2eeb05d16d66a407c054b5a63be85aaed4b0
Mohamed Khelif <mohamed.khelif@mongodb.com>
2026-01-05 15:00:42 -0500

    Handle non fialed status

--- src/api-server/routes/completions/parsley/chat.ts
+++ src/api-server/routes/completions/parsley/chat.ts
@@ -99,10 +99,14 @@ const chatRoute = async (
     });
     if (runResult.status === 'success') {
       requestContext.set('logURL', runResult.result);
-    } else if (runResult.status === 'failed') {
+    } else {
       logger.error('Error in get log file url workflow', {
         requestId: res.locals.requestId,
-        error: runResult.error,
+        status: runResult.status,
+        error:
+          runResult.status === 'failed'
+            ? runResult.error.message
+            : `Non Error Status: ${runResult.status}`,
       });
     }
   }
